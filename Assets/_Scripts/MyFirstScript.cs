87d0d449b4bf2f7b4c30cd89e154ad11dc88b92b
hadi chamli <hadinigma@gmail.com>
2026-02-13 17:06:22 +0200

    Background added + assets

--- Assets/_Scripts/MyFirstScript.cs
+++ Assets/_Scripts/MyFirstScript.cs
@@ -1,18 +1,14 @@
-using System;
 using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour
 {
-    private float xInput;
+    private float _xInput;
     [SerializeField] private float speed;
-    [SerializeField] private float jumpForce;
     private Rigidbody2D rb;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private GameObject groundCheck;
     [SerializeField] private float groundCheckRadius;
-    [SerializeField] private bool isGrounded;
-    [SerializeField] private int maxJumpCount;
-    private int _currJump;
+    [HideInInspector] public bool isGrounded;
 
     [SerializeField] private GameObject bulletPrefab;
 
@@ -23,44 +19,18 @@ public class MyFirstScript : MonoBehaviour
 
     private void Update()
     {
-        xInput = Input.GetAxisRaw("Horizontal");
+        _xInput = Input.GetAxisRaw("Horizontal");
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.transform.position, groundCheckRadius, groundLayer);
 
-        if (isGrounded)
-        {
-            _currJump = 0;
-        }
-
         if (Input.GetKeyDown(KeyCode.F))
         {
-            Instantiate(bulletPrefab, transform.position + new Vector3(1,0, 0), Quaternion.identity);
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Space) && _currJump < maxJumpCount)
-        {
-            Jump();
+            Instantiate(bulletPrefab, transform.position + new Vector3(1, 0, 0), Quaternion.identity);
         }
     }
-
     
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
-    }
-
-    // private void OnCollisionEnter2D(Collision2D other)
-    // {
-    //     if (other.gameObject.CompareTag("Platform"))
-    //     {
-    //         Destroy(other.gameObject, 1);
-    //     }
-    // }
-
-
-    private void Jump()
-    {
-        _currJump++;
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        rb.velocity = new Vector2(_xInput * speed, rb.velocity.y);
     }
 }
